1cecb8227f11f752f26edef7542e9996096c603e
SamuelMuloki <samuellebron0@gmail.com>
2017-07-07 08:48:41 +0300

    Functions for add, divide, multiply
    These functions are for adding, dividing and multiplying. They take in two numbers for example add(2,3) will print 5 in the console

---
+++ Add.js
@@ -0,0 +1,20 @@
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return x - y;
+}
+
+function divide(x, y){
+    return x / y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+console.log(add(2,3));
+console.log(subtract(5,2));
+console.log(divide(9,2));
+console.log(multiply(4,5));
